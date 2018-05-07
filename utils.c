55812c56a24fe513fb81db8e1a2d695493176970
Vova <vovavelho@gmail.com>
2018-05-07 22:30:54 +0300

    new: utils lib which contains utilities for pc

--- utils.c
+++ utils.c
@@ -1,6 +1,6 @@
 #include "utils.h"
 
-void delay (unsigned int secs) {
+void delay (unsigned int secs){
     unsigned int retTime = time(0) + secs;
-    while (time(0) < retTime);               // Loop until it arrives.
-}
+    while (time(0) < retTime);
+    }
