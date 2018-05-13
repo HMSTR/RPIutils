284b887d2e1cfa1b00f8ff70132e6a42b3874944
Vova <vovavelho@gmail.com>
2018-05-13 17:52:50 +0300

    now it must work

--- main.c
+++ main.c
@@ -5,7 +5,19 @@ void LEDExample(){
     LEDbroadcast(1);
 }
 
-
+#define LED 13
 int main(){
-    LEDExample();
+    //LEDExample();
+    wiringPiSetup();
+    pinMode (LED, OUTPUT);
+
+  for (;;)
+  {
+    digitalWrite (LED, HIGH) ;	// On
+    delay (500) ;		// mS
+    digitalWrite (LED, LOW) ;	// Off
+    delay (500) ;
+  }
+  return 0 ;
+}
 }
