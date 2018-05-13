93f9e7153c4aa53474232a684192d19d5a351517
Vova <vovavelho@gmail.com>
2018-05-13 23:13:07 +0300

    now we support 1-wire

--- main.c
+++ main.c
@@ -4,35 +4,13 @@
  
 using namespace std;
  
-double getTemp(OneWire * oneWire, uint64_t ds18b20s) {
-  uint8_t data[9];
- 
-  do {
-    oneWire->setDevice(ds18b20s);
-    oneWire->writeByte(CMD_CONVERTTEMP);
- 
-    delay(750);
- 
-    oneWire->setDevice(ds18b20s);
-    oneWire->writeByte(CMD_RSCRATCHPAD);
- 
-    for (int i = 0; i < 9; i++) {
-      data[i] = oneWire->readByte();
-    }
-  } while (oneWire->crc8(data, 8) != data[8]);
- 
-  return ((data[1] << 8) + data[0]) * 0.0625;
-}
- 
 int main() {
   OneWire * reader = new OneWire(22);
   try {
-    ds18b20->oneWireInit();
- 
+    reader->oneWireInit();
     uint64_t roms[n];
-    ds18b20->searchRom(roms, n);
+    reader->searchRom(roms, n);
     cout << "---------------------------------" << endl;
     cout << "devices = " << n << endl;
     cout << "---------------------------------" << endl;
- 
-}
+  }
