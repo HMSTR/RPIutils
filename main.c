15b9549868f095b6304a1d4d81874151d752a271
Vova <vovavelho@gmail.com>
2018-05-13 23:20:59 +0300

    now we support 1-wire

--- main.c
+++ main.c
@@ -6,7 +6,6 @@ using namespace std;
  
 int main() {
   OneWire * reader = new OneWire(22);
-  try {
     int n = 100;
     reader->oneWireInit();
     uint64_t roms[n];
@@ -14,7 +13,4 @@ int main() {
     cout << "---------------------------------" << endl;
     cout << "devices = " << n << endl;
     cout << "---------------------------------" << endl;
-  } catch(const std::exception& e){
-    ;
-  }
 }
