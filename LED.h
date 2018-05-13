469ea0e52c2ad003e2749997dc9fce98ca3fa5ae
Vova <vovavelho@gmail.com>
2018-05-13 17:37:45 +0300

    now it must work

--- LED.h
+++ LED.h
@@ -2,8 +2,12 @@
 #include <wiringPi.h>
 
 #define LED_RED 7
-#define LED_YEL 6
-#define LED_GRE 4
+#define LED_YEL 5
+#define LED_GRE 3
 #define LED_BL1 11
 #define LED_BL2 13
 #define LED_BL3 15
+
+
+void LEDbroadcast(int val);
+void LEDinit();
