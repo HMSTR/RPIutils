468cb793f776759703080a2651ce6bf390cfd53f
Vova <vovavelho@gmail.com>
2018-05-14 20:59:29 +0300

    1-wire added and fixed

--- main.c
+++ main.c
@@ -10,17 +10,17 @@
 // }
 // return 1;
 // }
-#include "1-wire.c"
+#include "1-wire.h"
 #include<iostream>
 using namespace std;
 int main(){
     OneWire *reader = new OneWire(6);
     reader->oneWireInit();
-    int n = 100;
-    uint64_t rom = reader->readRom();
-    cout << "---------------------------------" << endl;
-    cout << "rom = " << rom << endl;
-    cout << "---------------------------------" << endl;
-
+    uint64_t roms[n];
+    reader->searchRom(roms, n);
+    cout << "We've found "<< n << "roms"<<endl;
+    for (int i = 0; i < n; i++) {
+      cout << "addr T[" << (i + 1) << "] = " << roms[i] << endl;
+    }
     return 1;
 }
