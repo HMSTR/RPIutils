734aa74f837f0fcfd76d1a544b626c0c26524a39
Vova <vovavelho@gmail.com>
2018-05-14 20:05:39 +0300

    everything is fixed and stable

--- LED.h
+++ LED.h
@@ -2,11 +2,11 @@
 #include <wiringPi.h>
 
 #define LED_RED 7
-#define LED_YEL 5
-#define LED_GRE 3
-#define LED_BL1 11
-#define LED_BL2 13
-#define LED_BL3 15
+#define LED_YEL 9
+#define LED_GRE 8
+#define LED_BL1 0
+#define LED_BL2 2
+#define LED_BL3 3
 
 
 void LEDbroadcast(int val);
--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include "LED.h"
-
+#include "utils.h"
 int main(){
 LEDinit();
+while(1){
 LEDbroadcast(1);
+delay(1);
+LEDbroadcast(0);
 
+}
 return 1;
 }
